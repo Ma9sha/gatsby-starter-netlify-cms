8721545bcebc1ed1bc04ff2764d5d7c0b9b18c3a
Manisha Patel <ma9shap.gmail.com>
2019-08-30 15:26:15 +0100

    amended index.js, all.css, navbar.js files

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -17,16 +17,16 @@ export const IndexPageTemplate = ({
 }) => (
   <div>
     <div
-      className="full-width-image margin-top-0"
+      className="backgroundImage"
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+        // backgroundPosition: `top left`,
+        // backgroundAttachment: `fixed`,
       }}
     >
-      <div
+      {/* <div
         style={{
           display: 'flex',
           height: '150px',
@@ -35,34 +35,34 @@ export const IndexPageTemplate = ({
           alignItems: 'left',
           flexDirection: 'column',
         }}
-      >
+      > */}
         <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
+          className="firstBanner"
+          // style={{
+          //   boxShadow:
+          //     'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+          //   backgroundColor: 'rgb(255, 68, 0)',
+          //   color: 'white',
+          //   lineHeight: '1',
+          //   padding: '0.25em',
+          // }}
         >
           {title}
         </h1>
         <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
+          className="secondBanner"
+          // style={{
+          //   boxShadow:
+          //     'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+          //   backgroundColor: 'rgb(255, 68, 0)',
+          //   color: 'white',
+          //   lineHeight: '1',
+          //   padding: '0.25em',
+          // }}
         >
           {subheading}
         </h3>
-      </div>
+      {/* </div> */}
     </div>
     <section className="section section--gradient">
       <div className="container">
